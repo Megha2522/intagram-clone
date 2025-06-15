b0a5e5abd3c34f39171dd5fd6b574a40322641a4
Megha Mittal <megha@Meghas-MacBook-Pro.local>
2025-06-15 13:10:14 +0530

    day3-add controller , middlewares , routes

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import getDataUri from "../utils/dataUri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const register = async (req , res) => {
     try {
@@ -103,5 +105,109 @@ export const getProfile = async(req , res) => {
     }
 };
 
+export const editProfile = async(req , res) => {
+    try {
+        const userId = req.id;
+        const {bio , gender} = req.body;
+        const profitPicture = req.file;
+        let cloudResponse;
+
+        if(profitPicture) {
+            const fileUri = getDataUri(profilePicture);
+            cloudResponse = await cloudinary.uploader.upload(fileUri);
+        }
+
+        const user =  await User.findById(userId);
+
+        if(!user) {
+            return res.status(404).json({
+                message:'User not found.' , 
+                success: false
+            });
+        }
+        if(bio) user.bio = bio;
+        if(gender) user.gender = gender;
+        if(profitPicture) user.profilePicture = cloudResponse.secure_url;
+
+        await user.save();
+
+        return res.status(200).json({
+            message:'Profile updated.' , 
+            success: true ,
+            user
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getSuggestedUsers = async(req , res) => {
+    try {
+        const suggestedUsers = await User.find(_id:{$ne:req.id}).select("-password");
+
+        if(suggestedUsers) {
+            return res.status(400).json({
+                message:'Currently do not have any users.' ,
+            });
+        }
+        return res.status(200).json({
+            success:true ,
+            users: suggestedUsers
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+};
  
+export const followOrUnfollow = async (req , res) => {
+    try {
+        const followKrneWala = req.id; // me
+        const jiskoFollowKarungi = req.params.id; // my friend
+        if(followKrneWala === jiskoFollowKarungi) {
+            return res.status(400).json({
+                message: 'You cant follow or unfollow yourself' ,
+                success:false
+            });
+        }
+
+        const user = await User.findById(followKrneWala);
+        const targetUser = await User.findById(jiskoFollowKarungi);
+
+        if(!user || !targetUser) {
+            return res.status(400).json({
+                message:'User not found' ,
+                success: false
+            })
+        }
+
+        // mai check karunga ki follow krna hai ya unfollow
+        const isFollowing = user.following.includes(jiskoFollowKarungi);
+
+        if(isFollowing) {
+            //follow
+            await Promise.all([
+                User.updateOne({_id: followKrneWala }, { $push: { following: jiskoFollowKarungi } }) ,
+                User.updateOne({_id: jiskoFollowKarungi }, { $push: { followers: followKrneWala } }) ,
+            ])
+            return res.status(200).json({
+                message:'Followed successfully' ,
+                success: true
+            });
+        } else {
+            //unfollow
+            await Promise.all([
+                User.updateOne({_id: followKrneWala }, { $pull: { following: jiskoFollowKarungi } }) ,
+                User.updateOne({_id: jiskoFollowKarungi }, { $pull: { followers: followKrneWala } }) ,
+            ])
+            return res.status(200).json({
+                message:'Unfollowed successfully' ,
+                success: true
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
 
